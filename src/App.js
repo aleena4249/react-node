325b4dbd00ce3eda3af12db0dfb94761fae02f7e
Aleena <aleena@edgecorporation.net>
2022-05-26 11:37:10 +0400

    new react-node app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data1, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log("data", data1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {typeof data1 === "undefined" ? (
+        <p>Loading</p>
+      ) : (
+        data1.users &&
+        data1.users.map((user, i) => {
+          return <p key={i}>{user}</p>;
+        })
+      )}
     </div>
   );
 }
